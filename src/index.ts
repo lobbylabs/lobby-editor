c0fc027253b9617d08fe9e648c987fec8ae67533
Alex <zthh626@gmail.com>
2022-07-26 14:57:22 -0700

    fix: add exported components and downgrade tiptap

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 import { LobbyEditor, setContent, useLobbyEditor } from "./LobbyEditor";
+import { BubbleMenuButton } from "./Components/BubbleMenuButton";
+import { TableBubbleMenu } from "./Components/TableBubbleMenu";
+import { TextBubbleMenu } from "./Components/TextBubbleMenu";
 
-export { LobbyEditor, setContent, useLobbyEditor };
+export {
+  LobbyEditor,
+  setContent,
+  useLobbyEditor,
+  BubbleMenuButton,
+  TableBubbleMenu,
+  TextBubbleMenu,
+};
